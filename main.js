86a24f6e34ff6b6db1d2ce59f058888c047e356c
Kunihiro Ando <kxkx5150@users.noreply.github.com>
2021-02-19 10:41:58 +0900

    commit

---
+++ main.js
@@ -0,0 +1,41 @@
+document.getElementById("fileInput").addEventListener("change", (e) => {
+  const file = e.target.files[0];
+  if (!file) return;
+  var fileReader = new FileReader();
+  fileReader.onload = function () {
+    z80_init();
+    miracle_init();
+    miracle_reset();
+    loadRom(this.result);
+    start();
+  };
+  fileReader.readAsBinaryString(file);
+});
+window.addEventListener(
+  "resize",
+  (e) => {
+    resizeCanvas();
+  },
+  true
+);
+resizeCanvas = () => {
+  setTimeout(() => {
+    let canvas = document.getElementById("output");
+    const wh = window.innerHeight;
+    const ww = window.innerWidth;
+    const nw = 256;
+    const nh = 224;
+    const waspct = ww / wh;
+    const naspct = nw / nh;
+
+    if (waspct > naspct) {
+      var val = wh / nh;
+    } else {
+      var val = ww / nw;
+    }
+    let ctrldiv = document.querySelector(".ctrl_div");
+    canvas.style.height = 224 * val - ctrldiv.offsetHeight - 18 + "px";
+    canvas.style.width = 256 * val - 24 + "px";
+  }, 1200);
+};
+resizeCanvas();
